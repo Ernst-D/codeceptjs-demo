91ec5ef78d40c1f80bbc63c4255d8c44a201b8b2
Ernst-D <ernst.dzerevanko@gmail.com>
2022-05-16 21:59:12 +0400

    added comments regarding registration fixture flow

--- fixtures/registration.fixture.js
+++ fixtures/registration.fixture.js
@@ -1,9 +1,20 @@
 const assert = require("assert");
 const dayjs = require("dayjs");
 const LoginFixture = require("./login.fixture");
+
 /**
- * TODO: add description of registration + email check flow (use pre-created gmail and add unix timestamp)
- * @returns 
+ * NOTE: the point of testing registration with welcome email verification
+ * is that we either use mail server (a bit complex solution)
+ * or creating the testing Gmail inbox, because gmail has a small hack. 
+ * To avoid "This email is already registered" error - we can append a small text to gmail name (name+some_text@gmail.com)
+ * and therefore we can be sure that we won't get the error, but the message will be send at the same inbox.
+ * For example, we can add unix timestamp and it also will tell us exactly what message and when it was send.
+ * I used gmail tester for such purposes. The only "complex" thing - is to get token from Google API (it's a bit untrivial)
+ * @see https://www.npmjs.com/package/gmail-tester
+ */
+
+/**
+ * @returns {string} test gmail inbox with appended timestamp
  */
 const _testEmailInbox = () => `ourtestgmailinbox+${dayjs(new Date).unix()}@gmail.com`;
 
