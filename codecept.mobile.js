34ab3ac39e2ad893b8584b8c3d3b7a7a32e3079a
Ernst-D <ernst.dzerevanko@gmail.com>
2022-05-17 11:56:36 +0400

    init appium setup + changed configs

---
+++ codecept.mobile.js
@@ -0,0 +1,35 @@
+const { setHeadlessWhen, setCommonPlugins } = require('@codeceptjs/configure');
+
+// turn on headless mode when running with HEADLESS=true environment variable
+// export HEADLESS=true && npx codeceptjs run
+setHeadlessWhen(process.env.HEADLESS);
+
+// enable all common plugins https://github.com/codeceptjs/configure#setcommonplugins
+setCommonPlugins();
+
+exports.config = {
+  tests: 'tests/mobile/*.spec.js',
+  output: 'artifacts',
+  helpers: {
+    Appium: {
+      app: "./utils/dev-app.apk",
+      platform: "Android",
+      host:"localhost",
+      port:4723,
+      desiredCapabilities: {
+          "platformName": "Android",
+          "deviceName": "Pixel_4a",
+          "platformVersion": "11.0",
+          "automationName": "UiAutomator2",
+          "autoGrantPermissions": true
+      }
+    }
+  },
+  bootstrap: null,
+  mocha: {
+    reporterOptions: {
+      reportDir: "./artifacts/mochaReport"
+    }
+  },
+  name: 'test-task-codecept'
+};
