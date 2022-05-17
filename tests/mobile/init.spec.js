39f14162a2d0f545223aa0a2e7ee5c0c1e7d3b4a
Ernst-D <ernst.dzerevanko@gmail.com>
2022-05-18 01:43:29 +0400

    refactored mobile spec and moved waitForDisplayed to mobile utils

--- tests/mobile/init.spec.js
+++ tests/mobile/init.spec.js
@@ -1,28 +1,28 @@
+const MobileFixture = require("../../fixtures/mobile.fixture");
+const { _waitForDisplayed, textViewSelector } = require("../../utils/mobile.utils");
+
 Feature('Mobile');
 
 Scenario('Mobile test', ({ I }) => {
     I.seeAppIsInstalled("io.onesoil.scouting.staging");
     I.useWebDriverTo('To use WebdriverIO API', async ({ browser }) => {
-        // create new window
         /**
          * @type {WebdriverIO.Browser}
          */
         let driver = browser;
         const elem = async (selector) => await driver.$(selector); 
-        const textViewSelector = (matchedText) => `android=new UiSelector().text("${matchedText}").className("android.widget.TextView")`;
-        const editTextSelector = (matchedText) => `android=new UiSelector().text("${matchedText}").className("android.widget.EditText")`;
-        const waitForDisplayed = async (elem) => await (await elem).waitForDisplayed({ timeout:60000 });
+        let { welcomeScreen, loginScreen } = new MobileFixture(driver).setup();
 
-        const button = await elem(textViewSelector("I've already signed up. Sign in."));
-        await waitForDisplayed(button);
+        const button = await welcomeScreen.SignInTextView;
+        await _waitForDisplayed(button);
         await button.click();
-        await waitForDisplayed(await elem(editTextSelector("Enter e-mail address")));
-        await (await elem(editTextSelector("Enter e-mail address"))).setValue("ananas_parker@hotmail.com");
-        await (await elem(editTextSelector("Enter password"))).setValue("testTask132");
+        await _waitForDisplayed(loginScreen.EmailInput);
+        await loginScreen.EmailInput.setValue("ananas_parker@hotmail.com");
+        await loginScreen.PasswordInput.setValue("testTask132");
         
-        await (await driver.$$('android.widget.EditText')).pop().click();
-        await (await driver.$$('android.widget.EditText')).pop().pressKeyCode(66);
-        waitForDisplayed(await elem(textViewSelector("NEXT")));
+        await loginScreen.PasswordInput.click();
+        await loginScreen.PasswordInput.pressKeyCode(66);
+        _waitForDisplayed(await elem(textViewSelector("NEXT")));
         await (await elem(textViewSelector("NEXT"))).click();
      });
 });
--- utils/mobile.utils.js
+++ utils/mobile.utils.js
@@ -1,3 +1,9 @@
 const textViewSelector = (matchedText) => `android=new UiSelector().text("${matchedText}").className("android.widget.TextView")`;
 const editTextSelector = (matchedText) => `android=new UiSelector().text("${matchedText}").className("android.widget.EditText")`;
-module.exports = { textViewSelector, editTextSelector };
+
+/**
+ * @param {WebdriverIO.Element | Promise<WebdriverIO.Element>} elem 
+ */
+const _waitForDisplayed = async (elem) => await (await elem).waitForDisplayed({ timeout:60000 });
+
+module.exports = { textViewSelector, editTextSelector, _waitForDisplayed };
