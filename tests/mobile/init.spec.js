cceb34dc550186238c834b50558754ef101d758d
Ernst-D <ernst.dzerevanko@gmail.com>
2022-05-17 23:54:23 +0400

    added new steps for debug

--- tests/mobile/init.spec.js
+++ tests/mobile/init.spec.js
@@ -2,8 +2,23 @@ Feature('Mobile');
 
 Scenario('Mobile test', ({ I }) => {
     I.seeAppIsInstalled("io.onesoil.scouting.staging");
-    I.useWebDriverTo('open multiple windows', async ({ browser }) => {
+    I.useWebDriverTo('To use WebdriverIO API', async ({ browser }) => {
         // create new window
-        console.log(await browser.getContexts());
+        /**
+         * @type {WebdriverIO.Browser}
+         */
+        let driver = browser;
+        const elem = async (selector) => await driver.$(selector); 
+        const textViewSelector = (matchedText) => `android=new UiSelector().text("${matchedText}").className("android.widget.TextView")`;
+        const editTextSelector = (matchedText) => `android=new UiSelector().text("${matchedText}").className("android.widget.EditText")`;
+        const waitForDisplayed = async (elem) => await (await elem).waitForDisplayed({ timeout:60000 });
+        
+        const button = await elem(textViewSelector("I've already signed up. Sign in."));
+        await waitForDisplayed(button);
+        await button.click();
+        await waitForDisplayed(await elem(editTextSelector("Enter e-mail address")));
+        await (await elem(editTextSelector("Enter e-mail address"))).setValue("ananas_parker@hotmail.com");
+
+        await driver.pause(1000);
      });
 });
