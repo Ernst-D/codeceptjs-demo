2bf6c8f7e69cb0a50c64e912a7e8ca7909ee453c
Ernst-D <ernst.dzerevanko@gmail.com>
2022-05-18 00:32:16 +0400

    init login spec for mobile

--- tests/mobile/init.spec.js
+++ tests/mobile/init.spec.js
@@ -12,12 +12,17 @@ Scenario('Mobile test', ({ I }) => {
         const textViewSelector = (matchedText) => `android=new UiSelector().text("${matchedText}").className("android.widget.TextView")`;
         const editTextSelector = (matchedText) => `android=new UiSelector().text("${matchedText}").className("android.widget.EditText")`;
         const waitForDisplayed = async (elem) => await (await elem).waitForDisplayed({ timeout:60000 });
-        
+
         const button = await elem(textViewSelector("I've already signed up. Sign in."));
         await waitForDisplayed(button);
         await button.click();
         await waitForDisplayed(await elem(editTextSelector("Enter e-mail address")));
         await (await elem(editTextSelector("Enter e-mail address"))).setValue("ananas_parker@hotmail.com");
+        await (await elem(editTextSelector("Enter password"))).setValue("testTask132");
+        
+        await (await driver.$$('android.widget.EditText')).pop().click();
+        await (await driver.$$('android.widget.EditText')).pop().pressKeyCode(66);
+
 
         await driver.pause(1000);
      });
